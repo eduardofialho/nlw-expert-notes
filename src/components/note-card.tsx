8dabe488f9902ac68ae99204f6d4b87fc42e2012
Eduardo A. Fialho <e_fialho@outlook.com>
2024-02-07 14:48:43 -0300

    Aula 1

---
+++ src/components/note-card.tsx
@@ -0,0 +1,18 @@
+export function NoteCard() {
+  return (
+    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">há 2 dias</span>
+      <p className="text-sm leading-6 text-slate-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
+        officia voluptatem quos iste alias rerum nemo quae provident! Maxime
+        esse totam soluta dignissimos officia eum possimus facilis asperiores?
+        Ut, sed? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Distinctio dignissimos unde ducimus corrupti ratione quia nobis
+        provident reprehenderit, cupiditate in, vitae illo cum quibusdam
+        recusandae ipsam sed adipisci accusantium. Voluptas?
+      </p>
+
+      <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+    </button>
+  );
+}
